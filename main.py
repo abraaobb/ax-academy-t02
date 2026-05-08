42659f87a5709acfd21147a51ee1627f026dfe53
Eduardo Matheus <emrds.hef@uea.edu.br>
2026-05-08 16:44:03 -0400

    script corrigido - arquivo .json no lugar de lista direto no script

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 from playwright.sync_api import sync_playwright
-import os
+import os, json
 
 class Automacoes:
     
     def automacao7(self):
         os.makedirs("images", exist_ok=True)
 
+        with open("utils/sites.json", "r", encoding="utf-8") as file:
+            sites = json.load(file)
+
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=False, args=["--start-maximized"])
             context = browser.new_context(no_viewport=True)
 
-            sites = [
-                ("https://oglobo.globo.com/epoca/", "oglobo.png"),
-                ("https://www.espn.com.br/", "espn.png"),
-                ("https://www.acritica.com/", "acritica.png"),
-                ("https://www.cbc.ca/news", "cbc.png"),
-                ("https://www.linuxjournal.com/", "linux-journal.png")
-            ]
-
             pages = []
 
-            for url, image_name in sites:
+            for site in sites:
                 page = context.new_page()
-                page.goto(url, wait_until="domcontentloaded", timeout=60000)
-                page.wait_for_timeout(4000)
-                pages.append((page, image_name))
+                page.goto(site["url"], wait_until="domcontentloaded", timeout=50000)
+                page.wait_for_timeout(3000)
+                pages.append((page, site["image"]))
 
             for page, image_name in pages:
                 page.screenshot(path=f"images/{image_name}", full_page=True)
-                print(f"Captura da tela salvo em: images/{image_name}")
+
+                print(f"Prints sites salvo em: images/{image_name}")
 
             browser.close()
 
